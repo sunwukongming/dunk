815722f292aa381947501d4ea83f5bb46fa3913f
sunwukong <sunwukong@gmail.com>
2025-10-28 22:11:44 +0800

    feat: update button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex justify-center items-center gap-1 bg-contain bg-no-repeat bg-center cursor-pointer bg-[url('@/assets/images/svg/button-normal.svg')] hover:bg-[url('@/assets/images/svg/button-hover.svg')] disabled:bg-[url('@/assets/images/svg/button-disabled.svg')] disabled:cursor-not-allowed"
+    class="flex justify-center items-center gap-1 bg-contain bg-no-repeat bg-center outline-none cursor-pointer user-select-none bg-[url('@/assets/images/svg/button-normal.svg')] hover:bg-[url('@/assets/images/svg/button-hover.svg')] disabled:bg-[url('@/assets/images/svg/button-disabled.svg')] disabled:cursor-not-allowed"
   >
     <slot></slot>
   </button>
