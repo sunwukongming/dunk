0307d89eee4a8ea122006bb49506fab31e5b86dc
sunwukong <sunwukong@gmail.com>
2025-10-28 22:09:30 +0800

    feat: init

---
+++ src/components/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button
+    class="flex justify-center items-center gap-1 bg-contain bg-no-repeat bg-center cursor-pointer bg-[url('@/assets/images/svg/button-normal.svg')] hover:bg-[url('@/assets/images/svg/button-hover.svg')] disabled:bg-[url('@/assets/images/svg/button-disabled.svg')] disabled:cursor-not-allowed"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/views/Index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexView',
+})
+
+import background from '@/assets/images/background.png'
+import center from '@/assets/images/center.png'
+import Button from '@/components/Button.vue'
+</script>
+
+<template>
+  <div
+    :style="{ backgroundImage: `url(${background})` }"
+    class="bg-no-repeat bg-center bg-cover w-screen h-screen min-w-[1366px] min-h-[768px] flex justify-center"
+  >
+    <div class="mt-10 w-100 2xl:w-120">
+      <div class="flex">
+        <img class="grow" :src="center" />
+      </div>
+      <div class="mt-10 flex flex-col gap-1">
+        <div class="grid grid-cols-2 gap-6">
+          <Button class="grid-cols-1 aspect-3/1 grow">
+            <img class="size-6" src="@/assets/images/icon/dollar.svg" />
+            <div class="2xl:text-lg truncate">BUY $DUNK</div>
+          </Button>
+          <Button class="grid-cols-1 aspect-3/1 grow" disabled>
+            <img class="size-6" src="@/assets/images/icon/star.svg" />
+            <div class="2xl:text-lg truncate">FOLLOW TWITTER</div>
+          </Button>
+        </div>
+        <div class="grid grid-cols-2 gap-6">
+          <Button class="grid-cols-1 aspect-3/1 grow">
+            <img class="size-6" src="@/assets/images/icon/group-add.svg" />
+            <div class="2xl:text-lg truncate">JOIN TELEGRAM</div>
+          </Button>
+          <Button class="grid-cols-1 aspect-3/1 grow">
+            <img class="size-6" src="@/assets/images/icon/user-add.svg" />
+            <div class="2xl:text-lg truncate">JOIN DISCORD</div>
+          </Button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
